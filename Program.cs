443ee4d450b77b2fbf065396af98bfd513590d66
udees <udees@UDEESHA>
2025-08-21 09:17:12 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using PointsBet_Backend_Online_Code_Test;
+
+//Testing the method. TODO: Need to create Unit tests for this. Not done as not instructed to do. 
+
+//Test the array with valid quote
+string formattedText = StringFormatter.ToCommaSeparatedList(new string[] { "toyota", "honda", "BMW" }, "\"");
+
+Console.WriteLine($"Output: {formattedText}");
+
+//Test with null quote
+formattedText = StringFormatter.ToCommaSeparatedList(new string[] { "toyota", "honda", "BMW" }, null);
+
+Console.WriteLine($"Output: {formattedText}");
+
+//Test with null array
+try {
+    formattedText = StringFormatter.ToCommaSeparatedList(null, "\"");
+}
+catch( Exception ex) {
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+//Test with empty array
+try
+{
+    formattedText = StringFormatter.ToCommaSeparatedList(new string[] { }, "\"");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
---
+++ StringFormatter.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace PointsBet_Backend_Online_Code_Test
+{
+
+    /*
+    Improve a block of code as you see fit in C#.
+    You may make any improvements you see fit, for example:
+      - Cleaning up code
+      - Removing redundancy
+      - Refactoring / simplifying
+      - Fixing typos
+      - Any other light-weight optimisation
+    */
+    public class StringFormatter
+    {
+
+        /*
+         * Improvements made:
+         - Corrected typo error in method name ToCommaSeparatedList.
+         - Removed redundancy by removing StringBuilder contructor line and the loop. Used string.Join which is inbuild .net method for string.
+         - Using string.Join code is simplified and easy to read.
+         - Added null or empty validation for items array and return an ArgumentException to caller with proper error message.
+         - Instead of string.Format, Used lambda expression and string interpolation for better readerbility.
+         - Used null coalescing operator for quote prameter to build string without quotes when its null.
+         * */
+        public static string ToCommaSeparatedList(string[] items, string quote)
+        {
+            if (items == null || items.Length == 0)
+                throw new ArgumentException("Items array cannot be null or empty.", nameof(items));
+            else
+                return string.Join(",", items.Select(i => $"{quote ?? ""}{i}{quote ?? ""}"));
+                        
+        }
+    }
+}
